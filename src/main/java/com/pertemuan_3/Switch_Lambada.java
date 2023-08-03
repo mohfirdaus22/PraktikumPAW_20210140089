4aacbd68deffbcece51794e4be9587557d518421
mohfirdaus22 <thepashter6@gmail.com>
2023-08-03 21:48:41 +0700

    Repair Switch JAVA

--- src/main/java/com/pertemuan_3/Switch_Lambada.java
+++ src/main/java/com/pertemuan_3/Switch_Lambada.java
@@ -2,16 +2,32 @@ package com.pertemuan_3;
 
 public class Switch_Lambada {
     public static void main(String[] args) {
-        var nilai = "A";
+        int dayOfWeek = 3; // Misalnya, 1 untuk Senin, 2 untuk Selasa, dst.
 
-        switch (nilai) {
-            case "A" -> System.out.println("Wow anda lulus dengan baik");
-            case "B", "C" -> System.out.println("Anda Lulus");
-            case "D" -> System.out.println("Anda Tidak Lulus");
-            default -> {
-                System.out.println("Mungkin anda salah jurusan");
-            }
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Hari ini adalah Senin");
+                break;
+            case 2:
+                System.out.println("Hari ini adalah Selasa");
+                break;
+            case 3:
+                System.out.println("Hari ini adalah Rabu");
+                break;
+            case 4:
+                System.out.println("Hari ini adalah Kamis");
+                break;
+            case 5:
+                System.out.println("Hari ini adalah Jumat");
+                break;
+            case 6:
+                System.out.println("Hari ini adalah Sabtu");
+                break;
+            case 7:
+                System.out.println("Hari ini adalah Minggu");
+                break;
+            default:
+                System.out.println("Hari yang dimasukkan tidak valid!");
         }
     }
- 
 }
