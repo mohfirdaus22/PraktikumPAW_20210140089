716bc085554677070d8e561e17cef2e51dd7ec85
mohfirdaus22 <thepashter6@gmail.com>
2023-08-04 10:02:51 +0700

    Add ForLoop With POST Statement

---
+++ src/main/java/com/pertemuan_3/ForLoopPostStatement.java
@@ -0,0 +1,12 @@
+package com.pertemuan_3;
+
+public class ForLoopPostStatement {
+    public static void main(String[] args) {
+
+        for (var counter = 1; counter <= 10; counter++) {
+            System.out.println("Perulangan Ke-" + counter);
+
+        }
+
+    }
+}
