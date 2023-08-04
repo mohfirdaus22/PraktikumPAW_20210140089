cda896d9cfffc7d1e830883e45597f89e91b828d
mohfirdaus22 <thepashter6@gmail.com>
2023-08-04 09:47:34 +0700

    Add FoRLoop

---
+++ src/main/java/com/pertemuan_3/ForLoop.java
@@ -0,0 +1,11 @@
+package com.pertemuan_3;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for(;;){
+            System.out.println("Perulangan Tanpa Henti");
+        }
+
+    }
+}
