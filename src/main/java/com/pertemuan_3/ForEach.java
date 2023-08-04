6b212288b2dc0be28095019e2853e604264242ec
mohfirdaus22 <thepashter6@gmail.com>
2023-08-04 10:33:22 +0700

    Repair ForEach

---
+++ src/main/java/com/pertemuan_3/ForEach.java
@@ -0,0 +1,16 @@
+package com.pertemuan_3;
+
+public class ForEach {
+    public static void main(String[] args) {
+      
+        String[] array= {
+            "Eko", "Kurniawan", "Khannedy",
+            "Programmer", "Zaman", "Now"
+        };
+
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}
