3a6f04edb912aba6e2e3bb1d7c8cc83a3fa019dc
mohfirdaus22 <thepashter6@gmail.com>
2023-08-04 10:51:31 +0700

    Add Method Parameter

---
+++ src/main/java/com/pertemuan_3/MethodParameter.java
@@ -0,0 +1,10 @@
+package com.pertemuan_3;
+
+public class MethodParameter {
+    public static void main(String[] args) {
+        sayHello("Moh","Firdaus");
+      }
+      static void sayHello(String firstname, String Lastname){
+          System.out.println("Hello" + firstname + " " + Lastname);
+      }  
+}
