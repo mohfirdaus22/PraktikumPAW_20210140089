1341ba1e370cbf0392b4ca89666094dbcedc168f
mohfirdaus22 <thepashter6@gmail.com>
2023-08-04 09:59:58 +0700

    Add Forloop with Init Statement

--- src/main/java/com/pertemuan_3/ForLoopInitStatement.java
+++ src/main/java/com/pertemuan_3/ForLoopInitStatement.java
@@ -1,10 +1,9 @@
 package com.pertemuan_3;
 
-public class ForLoopCondition {
+public class ForLoopInitStatement {
     public static void main(String[] args) {
-        var counter = 1;
 
-        for(; counter <= 30;){
+        for(var counter = 1;counter <= 10;){
             System.out.println("Perulangan Ke-"+ counter);
             counter++;
         }
