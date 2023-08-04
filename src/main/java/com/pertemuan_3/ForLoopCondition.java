4ce88503a594c6df9c391f21d284be84a0960064
mohfirdaus22 <thepashter6@gmail.com>
2023-08-04 09:56:02 +0700

    Add ForLoop Condition

---
+++ src/main/java/com/pertemuan_3/ForLoopCondition.java
@@ -0,0 +1,13 @@
+package com.pertemuan_3;
+
+public class ForLoopCondition {
+    public static void main(String[] args) {
+        var counter = 1;
+
+        for(; counter <= 10;){
+            System.out.println("Perulangan Ke-"+ counter);
+            counter++;
+        }
+
+    }
+}
