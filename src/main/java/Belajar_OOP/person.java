753281aba419ede239444a1482139da4670ed17f
mohfirdaus22 <thepashter6@gmail.com>
2023-08-11 11:16:13 +0700

    Add constructor and method

--- src/main/java/Belajar_OOP/person.java
+++ src/main/java/Belajar_OOP/person.java
@@ -1,5 +1,22 @@
 package Belajar_OOP;
 
 public class person {
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    // Constructor
+    // person(String paramName, String paramAddress) {
+    // name = paramName;
+    // address = paramAddress;
+    // }
+
     
+    // alternatif Panggil
+    // var person = new person
+
+    // Method
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + " " + ", My Name is " + " " + name);
+    }
 }
